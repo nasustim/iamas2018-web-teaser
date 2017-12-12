1ac950091fb1387b7e26d65b009542df99f84ca5
Mitsuhiro HIBINO <hibino17@iamas.ac.jp>
2017-12-13 03:09:01 +0900

    fina

--- docs/script.js
+++ docs/script.js
@@ -1,3 +1,10 @@
 "use strict";
 
 (function () {})();
+
+$(window).scroll(function () {
+    var per = $(this).scrollTop() / $(document).innerHeight();
+    $("body").css({
+        "background-color": "rgba(100,100,100," + per + ")"
+    });
+});
--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
 (()=>{
 		
 })();
+
+$(window).scroll(function() {
+    let per = $(this).scrollTop() / $(document).innerHeight();
+    $("body").css({
+        "background-color": `rgba(100,100,100,${ per })`
+    });
+});
