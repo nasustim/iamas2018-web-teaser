b1a1e9fff14783ad63d0e186db9a59be551ebc63
Akihiro Kato <akihiro@LIFE-MAC-35noMacBook-Pro.local>
2017-12-18 19:39:02 +0900

    fix !!!requre -> del node_modules & npm install

--- js/script.js
+++ js/script.js
@@ -1,17 +1,31 @@
-(()=>{
+import $ from 'jquery';
+import fadeInElements from './_fadeInElements';
+
+(() => {
 
 
+  $("#container").css({
+    "height": $(window).height + "px"
+  });
+  $("#header").css({
+    "margin": `${$(window).height() - $("#title-block").height() - 20}px auto 20px auto`
+  });
+  $(window).on('resize', ()=>{
     $("#container").css({
-        "height": $(window).height + "px"
+      "height": $(window).height + "px"
     });
     $("#header").css({
-        "margin": `${$(window).height() - $("#title-block").height() - 20}px auto 20px auto`
+      "margin": `${$(window).height() - $("#title-block").height() - 20}px auto 20px auto`
     });
-})();
+  });
 
-$(window).scroll(function() {
+  fadeInElements('.js-fadein');
+
+  $(window).on('scroll', ()=> {
     let per = $(this).scrollTop() / $(document).innerHeight();
     $("#container").css({
-        "background-color": `rgba(255,255,255,${ per })`
+      "background-color": `rgba(255,255,255,${ per })`
     });
-});
+  });
+
+})();
